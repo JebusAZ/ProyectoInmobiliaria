d9eb632791c98a3957ddb9b9d72369d975af8572
Gibran9807 <84532008+Gibran9807@users.noreply.github.com>
2021-12-03 20:20:58 -0600

    Integracion de formulario de publicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PublishFormComponent } from './publish-form/publish-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublishFormComponent
   ],
   imports: [
     BrowserModule
