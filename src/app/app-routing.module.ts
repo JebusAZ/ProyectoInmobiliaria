2a196f957808bb8802b0e716d20bfdb33ea516c1
Gibran9807 <84532008+Gibran9807@users.noreply.github.com>
2021-12-05 21:58:45 -0600

    rutas mayormente añadidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ArticulosComponent } from './componentes/articulos/articulos.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PublishFormComponent } from './componentes/publish-form/publish-form.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { HomeComponent } from './componentes/home/home.component';
 
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'inmuebles', pathMatch:'full'},
+    {path:'inmuebles', component: ArticulosComponent},
+    {path:'producto', component: ProductoComponent},
+    {path:'login', component:LoginComponent},
+    {path:'form', component: PublishFormComponent},
+    {path:'nosotros', component:NosotrosComponent},
+    {path:'inicio', component:HomeComponent}
+]
+//no hagas push hasta que funcione
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
