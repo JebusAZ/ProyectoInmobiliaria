fdb5fdaea3c3d5ef386295cb06c16fac20de7b96
Ronaldo Barrientos Negrete <rbarrientosne@hotmail.com>
2021-12-09 12:02:25 -0600

    Login Firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import  firebase  from "firebase/compat/app"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  userloggedIn: boolean = false;
+  constructor(private auth: AngularFireAuth, private router: Router) { 
+    this.userloggedIn = false;
+
+    this.auth.onAuthStateChanged((user: any) =>{
+      if(user) {
+        this.userloggedIn = true;
+      } else{
+        this.userloggedIn = false;
+      }
+    });
+}
+
+loginUser(): Promise<any> {
+  return this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider).then((user: any) => {
+    console.log('Auth Service: loginUser: success', user);
+    return { isValid: true , message: user};
+  }).catch((err) => {
+    console.log('Auth Service: LoginUser: error', err)
+    return { isValid: false , message: err.code ? err.message : err};
+  });
+
+}
+
+getCurrentUser()
+{
+  return this.auth.currentUser;
+}
+
+logout() : Promise<void> {
+  return this.auth.signOut().then(() => {
+    this.router.navigate([''])
+  }).catch((err:any) => {
+
+  });
+}
+
 }
